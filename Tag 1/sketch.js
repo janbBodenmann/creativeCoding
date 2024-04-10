3dbf6640db34a8997b94c7357ccbcfc3d3fdb947
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-04-10 09:45:20 +0200

    set up days

---
+++ Tag 1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+ let test = createCanvas(windowHeight, windowWidth);
+     
+  circle(50,200,100)
+  
+}
+
+
+function draw() {
+  background(0, 0, 0, 10);
+
+}
---
+++ Tag 1/sketch2.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(windowHeight, windowWidth);
+    
+    circle(790,200,300)
+
+    }
+  
+  
+  function draw() {
+    background(0, 0, 0, 0);
+    
+  
+  
+  
+  }
+  
