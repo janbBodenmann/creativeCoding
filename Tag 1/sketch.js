c9020d7469d626c119e6dae5653b8e2c09e229ee
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-04-10 11:16:28 +0200

    square 1 finished

--- Tag 1/sketch.js
+++ Tag 1/sketch.js
@@ -1,12 +1,21 @@
 function setup() {
- let test = createCanvas(windowHeight, windowWidth);
-     
-  circle(50,200,100)
-  
+  createCanvas(500, 500);
+  background("gray");
+  noFill();
+  drawSquares();
 }
 
+function drawSquares() {
+  for (let i = 0; i < 50; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    square(x, y, random(400));
+  }
+}
 
-function draw() {
-  background(0, 0, 0, 10);
-
+function mousePressed() {
+  background("gray")
+  let randomColor = color(random(255), random(255), random(255));
+  stroke(randomColor);
+  drawSquares(); 
 }
--- Tag 1/sketch6.js
+++ Tag 1/sketch6.js
@@ -1,7 +1,7 @@
 function setup() {
     createCanvas(windowHeight, windowWidth);
     
-    rect(790,200,300)
+    circle(700,200,300)
 
     }
   
