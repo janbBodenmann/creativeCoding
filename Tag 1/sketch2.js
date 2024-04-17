e21d36521049ce4102cf0a1ac5ff8012ca07d282
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-04-17 11:32:14 +0200

    Task 1 finished

--- Tag 1/sketch2.js
+++ Tag 1/sketch2.js
@@ -5,10 +5,10 @@ function setup() {
   createCanvas(720, 620);
   pixelDensity(1);
 
-  // Bild laden
+  
   img = loadImage("../cat1.jpg");
 
-  // Erstelle den Skalierer (Slider)
+ 
   sizeSlider = createSlider(0, 700, 250);
   sizeSlider.position(30, 240);
 }
@@ -16,26 +16,24 @@ function setup() {
 function draw() {
   background(0);
 
-  // Bestimme die aktuelle Größe basierend auf dem Slider-Wert
   let newSize = sizeSlider.value();
 
-  // Anwenden der Skalierung
   scale(newSize / 250);
 
-  // Bild zeichnen
+
   image(img, 0, 0, width, height);
 
-  // Debug-Ausgabe des aktuellen Größenwerts
-  console.log("Neue Größe:", newSize);
+ 
+  console.log(newSize);
 
-  // Bildpixel bearbeiten basierend auf der Mausposition
+
   loadPixels();
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       let index = (x + y * width) * 4;
-      // Mausposition für die Farbkanäle verwenden
-      pixels[index + 1] = mouseX; // Grüner Kanal
-      pixels[index + 2] = mouseY; // Blauer Kanal
+     
+      pixels[index + 1] = mouseX; 
+      pixels[index + 2] = mouseY; 
     }
   }
   updatePixels();
--- Tag 1/sketch3.js
+++ Tag 1/sketch3.js
@@ -1,16 +1,43 @@
-function setup() {
-    createCanvas(windowHeight, windowWidth);
-    
-    circle(790,200,300)
 
+let img;
+let img1;
+
+function preload() {
+  img = loadImage("../grl2.jpg");
+  img1 = loadImage("../merkat.jpg");
+}
+
+function setup() {
+  createCanvas(1000, 1000);
+  pixelDensity(1);
+  background(0, 0, 0, 0);
+  image(img, 0 , 30, 600, 600);
+  
+
+  img.loadPixels();
+  img1.loadPixels();
+
+  for (let i = 0; i < img1.pixels.length; i += 4) {
+    if (img.pixels[i + 1] % 2 == 0) {
+      img1.pixels[i + 3] = 255;
+    } else {
+      img1.pixels[i + 3] = 0;
     }
-  
-  
-  function draw() {
-    background(0, 0, 0, 0);
-    
-  
-  
-  
   }
-  
+
+  img.updatePixels();
+  img1.updatePixels();
+  image(img1, 0, 30, 600, 650);
+
+}
+
+
+
+function draw() {
+
+
+}
+
+
+function draw() {}
+
