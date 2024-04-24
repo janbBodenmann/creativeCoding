42792d481805318fab0fefed826c35e3ccc22e60
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-04-24 11:39:40 +0200

    day3-task1-3

--- Tag 1/sketch4.js
+++ Tag 1/sketch4.js
@@ -1,16 +1,64 @@
-function setup() {
-    createCanvas(windowHeight, windowWidth);
-    
-    circle(790,200,300)
+let xPos, yPos;
+const stepSize = 10;
 
-    }
-  
-  
-  function draw() {
-    background(0, 0, 0, 0);
-    
-  
-  
-  
+function setup() {
+  createCanvas(400, 400);
+  xPos = width / 2;
+  yPos = height / 2;
+  frameRate(5); 
+}
+
+function draw() {
+  background(220);
+  moveAgent();
+  drawAgent();
+}
+
+function moveAgent() {
+  let direction = floor(random(0, 8));
+  switch (direction) {
+    case 0: // NORTH
+      yPos -= stepSize;
+      break;
+    case 1: // NORTHEAST
+      xPos += stepSize;
+      yPos -= stepSize;
+      break;
+    case 2: // EAST
+      xPos += stepSize;
+      break;
+    case 3: // SOUTHEAST
+      xPos += stepSize;
+      yPos += stepSize;
+      break;
+    case 4: // SOUTH
+      yPos += stepSize;
+      break;
+    case 5: // SOUTHWEST
+      xPos -= stepSize;
+      yPos += stepSize;
+      break;
+    case 6: // WEST
+      xPos -= stepSize;
+      break;
+    case 7: // NORTHWEST
+      xPos -= stepSize;
+      yPos -= stepSize;
+      break;
   }
+
   
+  xPos = constrain(xPos, 0, width);
+  yPos = constrain(yPos, 0, height);
+}
+
+function drawAgent() {
+  loadPixels(); 
+  clear(); 
+
+  let pixelColor = get(xPos, yPos); 
+  console.log(pixelColor);
+
+  fill(pixelColor); 
+  ellipse(xPos, yPos, 20, 20);
+}
--- Tag 1/sketch7.js
+++ Tag 1/sketch7.js
@@ -1,15 +1,16 @@
 let circles = [];
-const maxRadius = 50;
+const maxRadius = 40;
+const minRadius = 10;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
   let startCircle = createRandomCircle();
   circles.push(startCircle);
   drawCircle(startCircle);
 
   let counter = 0;
-  while (counter < 5000) {
-    let newCircle = createRandomCircle();
+  while (counter < 1000) {
+    const newCircle = createRandomCircle();
     if (!isOverlapping(newCircle)) {
       drawCircle(newCircle);
       circles.push(newCircle);
@@ -20,28 +21,43 @@ function setup() {
   }
 }
 
-function drawCircle(circle) {
+function drawCircle() {
   noStroke();
   fill(random(255), random(255), random(255), 100);
   ellipse(circle.x, circle.y, circle.radius * 2);
 }
-
 function createRandomCircle() {
   let x = random(width);
   let y = random(height);
-  let radius = random(10, maxRadius);
+  let radius = random(minRadius, maxRadius);
   return {
-    x: x,
-    y: y,
-    radius: radius,
+    x,
+    y,
+    radius,
   };
-}
-function isOverlapping(newCircle) {
-  for (let i = 0; i < circles.length; i++) {
-    const d = dist(newCircle.x, newCircle.y, circles[i].x, circles[i].y);
-    if (d < newCircle.radius + circles[i].radius) {
-      return true;
+    
+  
+  
+  function isOverlapping(){
+    for(let i = 0; i < circles.length; i++){
+        const d = dist(newCircle.x, newCircle.y, circles[i].x, circles[i].y);
+        if (d < newCircle.radius + circles[i].radius) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
