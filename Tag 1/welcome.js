bbc4ccb1a28b9cb8c5151defa0cddc9f878359ee
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-05-29 12:36:48 +0200

    sm

--- Tag 1/welcome.js
+++ Tag 1/welcome.js
@@ -1,1 +1,46 @@
+let number = 10; 
+let sizeSlider; 
 
+function setup() {
+  createCanvas(400, 400);
+  background(40, 30, 93);
+
+
+  sizeSlider = createSlider(1, 100, number); 
+  sizeSlider.position(10, height + 10); 
+  sizeSlider.input(updateNumber); 
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    onClick();
+  }
+}
+
+function onClick() {
+  let x = random(width);
+  let y = random(height);
+  let radius = random(10, 50);
+  fill(255);
+  noStroke();
+  circle(x, y, radius);
+}
+
+function updateNumber() {
+  number = sizeSlider.value(); 
+}
+
+function mouseClicked() {
+  createCircles(number); 
+}
+
+function createCircles(number) {
+  for (let i = 0; i < number; i++) {
+    let x = random(width);
+    let y = random(height);
+    let radius = random(10, 50);
+    fill(45, 128, 121);
+    noStroke();
+    circle(x, y, radius);
+  }
+}
